aaabb9cc1c7f6c6dec40bf39002b88af8f6e0d6f
krantonio <kingreiner.antonio@gmail.com>
2023-01-18 21:42:47 +0800

    setup project

---
+++ all-done/all-done/all_doneApp.swift
@@ -0,0 +1,17 @@
+//
+//  all_doneApp.swift
+//  all-done
+//
+//  Created by King Reiner Antonio on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct all_doneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
